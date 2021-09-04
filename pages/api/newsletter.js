c809b81f7e9ca15723c63108307a880cfb981e67
Manuel Figueira <manuel.figueira.9@gmail.com>
2021-09-04 13:19:29 -0300

    added error handling

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,18 +1,23 @@
 const handler = async (req, res) => {
-  if (req.method === 'POST') {
-    const { email } = req.body;
+  try {
+    if (req.method === 'POST') {
+      const { email } = req.body;
 
-    if (!email || email.trim() === '' || !email.includes('@')) {
-      res.status(422).json({ message: 'Invalid email address.' });
-      return;
+      if (!email || email.trim() === '' || !email.includes('@')) {
+        res.status(422).json({ message: 'Invalid email address.' });
+        return;
+      }
+
+      await fetch(process.env.NEXT_PUBLIC_NEWSLETTER_DB_ROUTE, {
+        method: 'POST',
+        body: JSON.stringify({ email }),
+      });
+
+      res.status(201).json({ message: 'Registered in Newsletter!' });
     }
-
-    await fetch(process.env.NEXT_PUBLIC_NEWSLETTER_DB_ROUTE, {
-      method: 'POST',
-      body: JSON.stringify({ email }),
-    });
-
-    res.status(201).json({ message: 'Registered in Newsletter!' });
+  } catch (err) {
+    console.log('err :>> ', err);
+    res.status(500).json({ message: 'An error has ocurred.' });
   }
 };
 
